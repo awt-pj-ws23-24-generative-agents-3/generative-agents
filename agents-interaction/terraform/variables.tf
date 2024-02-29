67252c94e55768529555e168aecf418725453620
Siar <siarakbayin@gmail.com>
2024-02-29 21:38:55 +0100

    Added missing Terraform files

--- agents-interaction/terraform/variables.tf
+++ agents-interaction/terraform/variables.tf
@@ -1,10 +1,12 @@
 variable "vpc_id" {
   type        = string
   description = "AWS VPC ID (using default is recommended)"
+  default = "vpc-0f2d9adf03961f221"
 }
 
 variable "subnet_id" {
   type        = string
   description = "AWS Subnet ID (using default is recommended)"
+    default = "subnet-028b97041fb0039d2"
 }
 
